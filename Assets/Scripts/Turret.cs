3fdee607419e76f07bbc989f6e20e5c431756bc7
shanart <shanin.x@gmail.com>
2021-04-11 20:40:02 +0300

    CU-h10u97 Turrest find and lock target

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class Turret : MonoBehaviour
 {
-    public Transform target;
+    private Transform target;
     public float range = 15f;
-
+    public float turnSpeed = 10f;
     public string enemyTag = "Enemy";
+    public Transform partToRotate;
 
     // Start is called before the first frame update
     void Start()
@@ -18,19 +19,38 @@ public class Turret : MonoBehaviour
     void UpdateTarget()
     {
         GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
-        float shortestDistance = Mathf.Infinity;
+		float shortestDistance = Mathf.Infinity;
+		GameObject nearestEnemy = null;
+        
+		foreach (GameObject enemy in enemies)
+		{
+			float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+			if (distanceToEnemy < shortestDistance)
+			{
+				shortestDistance = distanceToEnemy;
+				nearestEnemy = enemy;
+			}
+		}
 
-
-        foreach (var enemy in enemies)
-        {
-            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
-
-        }
+		if (nearestEnemy != null && shortestDistance <= range)
+		{
+			target = nearestEnemy.transform;
+		} else
+		{
+			target = null;
+		}
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (target == null)
+            return;
+
+        Vector3 dir = target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+        Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
+        partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
         
     }
 
