26b64f0430e06dbe6fdd788d940f861341d2fec4
shanart <shanin.x@gmail.com>
2021-04-11 07:51:24 +0300

    updates

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public float speed = 10f;
+    private Transform target;
+    private int wavepointIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = Waypoints.points[0];
+    }   
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        Move object to the waypoint
+        */
+        Vector3 direction = target.position - transform.position;
+        transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+
+        
+    }
+}
---
+++ Assets/Scripts/Waypoints.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Waypoints : MonoBehaviour
+{
+    public static Transform[] points;
+
+    // // Start is called before the first frame update
+    void Awake()
+    {
+        points = new Transform[transform.childCount];
+        for (int i = 0; i < points.Length; i++)
+        {
+            points[i] = transform.GetChild(i);
+        }
+    }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+}
